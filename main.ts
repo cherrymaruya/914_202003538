7001e32492f7035fa68cc270cf328ba265f31319
cherrymaruya <113489368+cherrymaruya@users.noreply.github.com>
2022-09-14 11:43:51 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,58 @@
+input.onButtonPressed(Button.A, function () {
+    steps = 0
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showString("RESET")
+})
+input.onGesture(Gesture.Shake, function () {
+    steps += 1
+    basic.showNumber(steps)
+    if (steps == 10) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.setVolume(130)
+    }
+    if (steps == 20) {
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.setVolume(130)
+    }
+    if (steps == 30) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.setVolume(130)
+    }
+    if (steps == 40) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+        music.setVolume(130)
+    }
+    if (steps == 50) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.setVolume(130)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(200)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0.7 * steps)
+    basic.showString("M")
+    for (let index = 0; index < 2; index++) {
+        basic.showString("Cheer up!")
+    }
+})
+let steps = 0
+music.playMelody("A B C5 B A A A B ", 120)
+music.playMelody("B B A A A A B C5 ", 120)
+steps = 0
+basic.showString("Ready?")
